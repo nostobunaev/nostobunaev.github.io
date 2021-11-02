51dd1984463daa9917133f9a11c9a00e9c59360a
nostobunaev <nostobunaev@alaska.edu>
2021-11-01 16:20:32 -0800

    Second Draft

---
+++ ServiceC.js
@@ -0,0 +1,51 @@
+function checkForm() {
+    let title = document.getElementById("ftitle")
+    let desc = document.getElementById("fdescription")
+    let price = document.getElementById("fprice")
+    let error = document.getElementById("formErrors")
+    let errorsFound = false;
+
+    if (!title.value) {
+        errorsFound = true;
+        let errorTitle = document.createElement("li");
+        errorTitle.textContent = "Missing title.";
+        error.appendChild(errorTitle);
+    }
+
+    if (title.value.length >= 30) {
+        errorsFound = true;
+        let errorTitle2 = document.createElement("li");
+        errorTitle2.textContent = "Titile must be less than 30 characters.";
+        error.appendChild(errorTitle2);
+    }
+
+    if (!desc.value) {
+        errorsFound = true;
+        let errorDesc = document.createElement("li");
+        errorDesc.textContent = "Missing description.";
+        error.appendChild(errorDesc);
+    }
+
+    if (title.value.length >= 500) {
+        errorsFound = true;
+        let errorDesc2 = document.createElement("li");
+        errorDesc2.textContent = "Titile must be less than 500 characters.";
+        error.appendChild(errorDesc2);
+    }
+
+    if (!price.value) {
+        errorsFound = true;
+        let errorPrice = document.createElement("li");
+        errorPrice.textContent = "Missing price.";
+        error.appendChild(errorPrice);
+    }
+    
+    if (errorsFound === false) {
+        error.classList.add('hide');
+    }
+}
+
+document.getElementById("submit").addEventListener("click", function(event) {
+    checkForm();
+    event.preventDefault();
+});
---
+++ register.js
@@ -0,0 +1,79 @@
+function checkForm() {
+    let firstName = document.getElementById("firstName");
+    let secondName = document.getElementById("secondName");
+    let email = document.getElementById("email");
+    let password = document.getElementById("password");
+    let passwordConfirm = document.getElementById("passwordConfirm");
+    let error = document.getElementById("formErrors");
+    let errorsFound = false;
+
+    if (!firstName.value) {
+        errorsFound = true;
+        let errorName = document.createElement("li");
+        errorName.textContent = "Missing first name.";
+        error.appendChild(errorName);
+     }
+    
+     if (!secondName.value) {
+        errorsFound = true;
+        let errorName = document.createElement("li");
+        errorName.textContent = "Missing second name.";
+        error.appendChild(errorName);
+     }
+  
+     if (!email.value || !/^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,5}$/.test(email.value)) {
+        errorsFound = true;
+        let errorEmail = document.createElement("li");
+        errorEmail.textContent = "Invalid or missing email address.";
+        error.appendChild(errorEmail);
+     }
+  
+     if (password.value.length < 10 || password.value.length > 20) {
+        errorsFound = true;
+        let errorPass = document.createElement("li");
+        errorPass.textContent = "Password must be between 10 and 20 characters.";
+        error.appendChild(errorPass);
+     }
+  
+     if (!password.value || !/[a-z]/.test(password.value)) {
+        errorsFound = true;
+        let errorPass2 = document.createElement("li");
+        errorPass2.textContent = "Password must contain at least one lowercase character.";
+        error.appendChild(errorPass2);
+     }
+  
+     if (!password.value || !/[A-Z]/.test(password.value)) {
+        errorsFound = true;
+        let errorPass3 = document.createElement("li");
+        errorPass3.textContent = "Password must contain at least one uppercase character.";
+        error.appendChild(errorPass3);
+     }
+  
+     if (!password.value || !/[0-9]/.test(password.value)) {
+        errorsFound = true;
+        let errorPass4 = document.createElement("li");
+        errorPass4.textContent = "Password must contain at least one digit.";
+        error.appendChild(errorPass4);
+     }
+  
+     if (passwordConfirm && password.value !== passwordConfirm.value) {
+        errorsFound = true;
+        let errorPass5 = document.createElement("li");
+        errorPass5.textContent = "Password and confirmation password don't match.";
+        error.appendChild(errorPass5);
+     }
+  
+     if (errorsFound === false) {
+        error.classList.add('hide');
+     }
+  } 
+  
+document.getElementById("submit").addEventListener("click", function(event) {
+    if (error.length > 1000) {
+        error.innerHTML = '';
+    }
+    else {
+        checkForm();
+        event.preventDefault();
+    }
+});
